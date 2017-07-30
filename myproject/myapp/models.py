235597d8edec8c55cc681dd400bad4f7574d52ec
vishavgupta97 <vishavgupta988@gmail.com>
2017-07-30 16:07:31 +0530

    certain modifications done

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -45,6 +45,12 @@ class PostModel(models.Model):
     def comments(self):
         return CommentModel.objects.filter(post=self).order_by('-created_on')
 
+    @property
+
+    def categories(self):
+
+        return CategoryModel.objects.filter(post=self)
+
 
 class LikeModel(models.Model):
     user = models.ForeignKey(UserModel)
